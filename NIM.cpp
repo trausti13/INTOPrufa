440fac33a41036596a85f40360cb68ecec16bfd2
Notandi <tgj@ubuntu.(none)>
2013-10-17 22:23:43 +0000

    ALL YOUR BASE

--- NIM.cpp
+++ NIM.cpp
@@ -35,6 +35,7 @@ int main( )
 	do
 	{
 		cout << endl << endl << endl;
+		cout << "ALL YOUR BASER ARE BELONG TO US! (trausti13)" << endl;
 		cout << "                ADALVALMYND " << endl << endl;
 		cout << "                      1. Spila NIM" << endl;
 		cout << "                      2. Birta reglurnar i NIM" << endl;
